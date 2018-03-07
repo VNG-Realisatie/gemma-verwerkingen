fbed1890ba197382126137ba510ed95b92294b8b
Joeri Bekker <joeri@maykinmedia.nl>
2018-03-07 16:22:17 +0100

    Various model changes based on first feedback with Johan Boer (VNG).

--- poc/src/verwerkingsregister/datamodel/admin.py
+++ poc/src/verwerkingsregister/datamodel/admin.py
@@ -8,7 +8,7 @@ from .models import (Applicatie, DataElement, DataObject, Doel, Grondslag,
 
 @admin.register(Organisatie)
 class OrganisatieAdmin(admin.ModelAdmin):
-    list_display = ('identificatie', 'naam', )
+    list_display = ('naam', )
 
 
 @admin.register(Doel)
@@ -18,7 +18,7 @@ class DoelAdmin(admin.ModelAdmin):
 
 @admin.register(Grondslag)
 class GrondslagAdmin(admin.ModelAdmin):
-    list_display = ('identificatie', 'get_omschrijving_display', )
+    list_display = ('get_omschrijving_display', )
 
     def get_omschrijving_display(self, obj):
         return '{}...'.format(obj.omschrijving[0:100])
@@ -26,33 +26,37 @@ class GrondslagAdmin(admin.ModelAdmin):
 
 @admin.register(Proces)
 class ProcesAdmin(admin.ModelAdmin):
-    list_display = ('identificatie', 'naam', 'verwerkings_verantwoordelijke', )
+    list_display = ('naam', 'verwerkings_verantwoordelijke', )
     list_filter = ('organisatie', )
 
 
 @admin.register(ProcesStap)
 class ProcesStapAdmin(admin.ModelAdmin):
-    list_display = ('identificatie', 'naam', 'bewaartermijn', 'proces', 'realiseert', 'get_gebaseerd_op_display', )
+    list_display = ('pk', 'naam', 'bewaartermijn', 'proces', 'realiseert', 'get_gebaseerd_op_display', )
     list_filter = ('proces__organisatie', )
 
     def get_gebaseerd_op_display(self, obj):
-        return ', '.join(list(obj.gebaseerd_op.values_list('identificatie', flat=True)))
+        return '<br>'.join([str(o) for o in obj.gebaseerd_op.all()])
+    get_gebaseerd_op_display.allow_tags = True
+    get_gebaseerd_op_display.short_description = _('gebaseerd op')
 
 
 class DataElementInline(admin.TabularInline):
     model = DataElement
+    extra = 0
+    # filter_horizontal = ('process_stap', )
 
 
 @admin.register(DataObject)
 class DataObjectAdmin(admin.ModelAdmin):
     list_display = ('naam', )
-    list_filter = ('organisatie', )
+    list_filter = ('bronhouder', )
     inlines = (DataElementInline, )
 
 
 @admin.register(Applicatie)
 class ApplicatieAdmin(admin.ModelAdmin):
-    list_display = ('identificatie', 'naam', )
+    list_display = ('naam', )
 
 
 @admin.register(Rol)
@@ -63,9 +67,9 @@ class RolAdmin(admin.ModelAdmin):
 
 @admin.register(Medewerker)
 class MedewerkerAdmin(admin.ModelAdmin):
-    list_display = ('identificatie', 'naam', )
+    list_display = ('naam', )
 
 
 @admin.register(MedewerkerRol)
 class MedewerkerRolAdmin(admin.ModelAdmin):
-    list_display = ('naam', 'gedefinieerd_door', 'wordt_vervuld_door', 'is_geautoriseerd_voor', )
+    list_display = ('naam', 'gedefinieerd_voor', 'wordt_vervuld_door', 'is_geautoriseerd_voor', )
--- poc/src/verwerkingsregister/datamodel/models.py
+++ poc/src/verwerkingsregister/datamodel/models.py
@@ -1,16 +1,19 @@
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from djchoices import DjangoChoices, ChoiceItem
 
 
 class Organisatie(models.Model):
-    identificatie = models.CharField(max_length=12)
     naam = models.CharField(max_length=255)
     adres = models.TextField(blank=True)
 
     class Meta:
         verbose_name = _('organisatie')
         verbose_name_plural = _('organisaties')
+
+    def __str__(self):
+        return self.naam
 
 
 class Doel(models.Model):
@@ -21,18 +24,22 @@ class Doel(models.Model):
         verbose_name = _('doel')
         verbose_name_plural = _('doelen')
 
+    def __str__(self):
+        return self.naam
+
 
 class Grondslag(models.Model):
-    identificatie = models.CharField(max_length=12)
     omschrijving = models.TextField(blank=True)
 
     class Meta:
         verbose_name = _('grondslag')
         verbose_name_plural = _('grondslagen')
 
+    def __str__(self):
+        return '{}...'.format(self.omschrijving[0:50])
+
 
 class Proces(models.Model):
-    identificatie = models.CharField(max_length=12)
     naam = models.CharField(max_length=300)
     verwerkings_verantwoordelijke = models.CharField(max_length=100)
 
@@ -42,57 +49,79 @@ class Proces(models.Model):
         verbose_name = _('proces')
         verbose_name_plural = _('processen')
 
+    def __str__(self):
+        return self.naam
+
 
 class ProcesStap(models.Model):
-    identificatie = models.CharField(max_length=12)
     naam = models.CharField(max_length=255)
     bewaartermijn = models.PositiveSmallIntegerField()
 
     proces = models.ForeignKey(Proces, related_name='bestaat_uit', on_delete=models.CASCADE)
-    realiseert = models.ForeignKey(Doel, on_delete=models.CASCADE)
-    gebaseerd_op = models.ManyToManyField(Grondslag)
+    realiseert = models.ForeignKey(Doel, verbose_name=_('realiseert (doel)'), on_delete=models.CASCADE)
+    gebaseerd_op = models.ManyToManyField(Grondslag, verbose_name=_('gebaseerd op (grondslag)'))
 
     class Meta:
         verbose_name = _('proces stap')
         verbose_name_plural = _('proces stappen')
 
+    def __str__(self):
+        return self.naam
+
 
 class DataObject(models.Model):
     naam = models.CharField(max_length=255)
 
-    organisatie = models.ForeignKey(Organisatie, related_name='is_bronhouder_van', on_delete=models.CASCADE)
+    bronhouder = models.ForeignKey(Organisatie, verbose_name=_('bronhouder (organisatie)'), related_name='is_bronhouder_van', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = _('data object')
         verbose_name_plural = _('data objecten')
 
+    def __str__(self):
+        return self.naam
+
 
 class DataElement(models.Model):
     naam = models.CharField(max_length=255)
 
-    process_stap = models.ForeignKey(ProcesStap, related_name='mag_bewerken', on_delete=models.CASCADE)
+    process_stap = models.ManyToManyField(ProcesStap, related_name='mag_bewerken')
     data_object = models.ForeignKey(DataObject, related_name='omvat', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = _('data element')
         verbose_name_plural = _('data elementen')
 
+    def __str__(self):
+        return self.naam
+
 
 class Applicatie(models.Model):
-    identificatie = models.CharField(max_length=12)
     naam = models.CharField(max_length=255)
 
     class Meta:
         verbose_name = _('applicatie')
         verbose_name_plural = _('applicaties')
 
+    def __str__(self):
+        return self.naam
+
+
+class RolName(DjangoChoices):
+    gegevensontvanger = ChoiceItem(label=_('Gegevensontvanger'))
+    gegevensaanbieder = ChoiceItem(label=_('Gegevensaanbieder'))
+    gegevensverwerker = ChoiceItem(label=_('Gegevensverwerker'))
+    eventontvanger = ChoiceItem(label=_('Eventontvanger'))
+    eventaanbieder = ChoiceItem(label=_('Eventaanbieder'))
+    eventverwerker = ChoiceItem(label=_('Eventverwerker'))
+
 
 class Rol(models.Model):
-    naam = models.CharField(max_length=255)
+    naam = models.CharField(max_length=255, choices=RolName.choices)
 
-    wordt_vervuld_door_organisatie = models.ForeignKey(Organisatie, null=True, on_delete=models.CASCADE)
-    wordt_vervuld_door_applicatie = models.ForeignKey(Applicatie, null=True, on_delete=models.CASCADE)
-    heeft_betrekking_op = models.ForeignKey(ProcesStap, on_delete=models.CASCADE)
+    wordt_vervuld_door_organisatie = models.ForeignKey(Organisatie, verbose_name=_('wordt vervuld door (organisatie)'), blank=True, null=True, on_delete=models.CASCADE)
+    wordt_vervuld_door_applicatie = models.ForeignKey(Applicatie, verbose_name=_('wordt vervuld door (applicatie)'), blank=True, null=True, on_delete=models.CASCADE)
+    heeft_betrekking_op = models.ForeignKey(ProcesStap, verbose_name=_('heeft betreking op (proces stap)'), on_delete=models.CASCADE)
 
     def clean(self):
         if self.wordt_vervuld_door_applicatie and self.wordt_vervuld_door_applicatie:
@@ -102,23 +131,31 @@ class Rol(models.Model):
         verbose_name = _('rol')
         verbose_name_plural = _('rollen')
 
+    def __str__(self):
+        return self.naam
+
 
 class Medewerker(models.Model):
-    identificatie = models.CharField(max_length=12)
     naam = models.CharField(max_length=255)
 
     class Meta:
         verbose_name = _('medewerker')
         verbose_name_plural = _('medewerkers')
 
+    def __str__(self):
+        return self.naam
+
 
 class MedewerkerRol(models.Model):
     naam = models.CharField(max_length=255)
 
-    gedefinieerd_door = models.ForeignKey(Applicatie, on_delete=models.CASCADE)
-    wordt_vervuld_door = models.ForeignKey(Medewerker, on_delete=models.CASCADE)
-    is_geautoriseerd_voor = models.ForeignKey(DataObject, on_delete=models.CASCADE)
+    gedefinieerd_voor = models.ForeignKey(Applicatie, verbose_name=_('gedefinieerd voor (applicatie)'), on_delete=models.CASCADE)
+    wordt_vervuld_door = models.ForeignKey(Medewerker, verbose_name=_('wordt vervuld door (medewerker)'), on_delete=models.CASCADE)
+    is_geautoriseerd_voor = models.ForeignKey(DataObject, verbose_name=_('is geautoriseerd voor (data object)'), on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = _('medewerker rol')
         verbose_name_plural = _('medewerker rollen')
+
+    def __str__(self):
+        return self.naam
