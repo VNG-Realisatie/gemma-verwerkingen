c6b131be9c75bd242bb0c46bf661ddb169505d39
Bart Jeukendrup <bart@jeukendrup.nl>
2018-03-07 19:55:44 +0100

    Include Docker assets

--- poc/src/verwerkingsregister/wsgi.py
+++ poc/src/verwerkingsregister/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "verwerkingsregister.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "verwerkingsregister.conf.settings")
 
 application = get_wsgi_application()
