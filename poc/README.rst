98aea13a42241c5a461aa3b471d1b3aea2a64240
Joeri Bekker <joeri@maykinmedia.nl>
2018-03-07 13:56:05 +0100

    Initial project setup.

---
+++ poc/README.rst
@@ -0,0 +1,12 @@
+===================
+Verwerkingsregister
+===================
+
+:Version: 0.0.1
+:Source: https://github.com/VNG-Realisatie/gemma-verwerkingen
+:Keywords: verwerkingen, verwerkingsregister, common grounds, nlx
+
+
+Proof of concept of a "verwerkingsregister", developed during the Common
+Grounds fieldlab.
+
---
+++ poc/src/verwerkingsregister/conf/local_example.py
@@ -0,0 +1,34 @@
+"""
+Any machine specific settings when using development settings.
+"""
+
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+DJANGO_PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+BASE_DIR = os.path.abspath(os.path.join(DJANGO_PROJECT_DIR, os.path.pardir, os.path.pardir))
+
+
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+DATABASES = {
+    'default': {
+        # 'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',  # Set to empty string for default.
+    }
+}
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'ec_w%f)(6t3_-y17oo!lkh-9x@5)_jdp)(n68hvj^$rrhrjj-o'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Add the hostnames or IP addresses that access this web application here.
+ALLOWED_HOSTS = []
